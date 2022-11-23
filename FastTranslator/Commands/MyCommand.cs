21832a1a79e7e3173f42f716727f4d7c38a40418
Artem <kor.artemik@yandex.ru>
2022-11-23 10:23:29 +0300

    feat: add plugin

---
+++ FastTranslator/Commands/MyCommand.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+
+namespace FastTranslator
+{
+    [Command(PackageIds.MyCommand)]
+    internal sealed class MyCommand : BaseCommand<MyCommand>
+    {
+        protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
+        {
+            var docView = await VS.Documents.GetActiveDocumentViewAsync();
+            var selection = docView?.TextView.Selection.SelectedSpans.FirstOrDefault();
+
+            if (selection.HasValue)
+            {
+                VsShellUtilities.OpenBrowser("https://translate.google.com/?hl=ru&sl=en&tl=ru&text=" + selection.Value.ToString());
+            }
+        }
+    }
+}
